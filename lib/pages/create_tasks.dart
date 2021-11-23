fd6aa25f331db5a977df1431fa9ae9d12501b55b
cjherte1 <92050057+cjherte1@users.noreply.github.com>
2021-11-23 04:30:03 -0600

    can complete or delete tasks. added a compeleted tasks list to user in case we want to display completed tasks. also added 5 points to user points field every time a task is completed, displayed in achievements

--- lib/pages/create_tasks.dart
+++ lib/pages/create_tasks.dart
@@ -5,19 +5,19 @@ import '../database_helper.dart';
 import 'package:date_time_picker/date_time_picker.dart';
 import 'package:intl/intl.dart';
 
-
 /*
 CREATES A CARD WIDGET FOR EACH TASK
 just formats the task information into a box
  */
-Widget taskCard(task) {
+Widget taskCard(Task task) {
   DateTime dt = DateTime.parse(task.datetime);
-  String formattedDate = DateFormat('MMMM d, yyyy - h:mm a').format(dt).toString();
+  String formattedDate =
+      DateFormat('MMMM d, yyyy - h:mm a').format(dt).toString();
   return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
+    margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+    child: Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Text(
@@ -35,9 +35,9 @@ Widget taskCard(task) {
                 color: Colors.grey[800],
               ),
             ),
-          ],
-        ),
-      ));
+          ]),
+    ),
+  );
 }
 
 class CreateTasks extends StatefulWidget {
@@ -65,66 +65,70 @@ class _CreateTasksState extends State<CreateTasks> {
   @override
   Widget build(BuildContext context) {
     final currentUser = ModalRoute.of(context)!.settings.arguments as User;
-    //List utasks = currentUser.tasks.where((isCompleted) => true).toList();
-    // Future<List<Task>> tasks = getTasks(currentUser);
-    // return FutureBuilder<List<Task>>(
-    //   future: tasks,
-    //   builder: (context, snapshot) {
-    //     if (snapshot.connectionState == ConnectionState.done) {
-    return ListView(children: [
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Welcome ' + currentUser.firstName + '!',
-            style: const TextStyle(
-              fontSize: 16,
-            ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Column(
-            //TO DO: ONLY SHOW TAKS FOR THAT DAY
-            children: currentUser.tasks.map((task) => taskCard(task)).toList(),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          ListView(
-            padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            children: <Widget>[
-              pressed ? const CreateTasksForm() : const SizedBox(),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: const Color(0xFFF29765),
-                    textStyle: const TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold)),
-                child: pressed ? const Text("Go Back") : const Text("Add Task"),
-                onPressed: () {
-                  setState(() {
-                    pressed = !pressed;
-                  });
-                },
-              )
-            ],
-          ),
-        ],
+    List<Task> currentTasks = currentUser.tasks;
+
+    return Column(children: [
+      Expanded(
+        child: ListView.builder(
+          itemCount: currentUser.tasks.length,
+          itemBuilder: (context, index) {
+            return Column(children: [
+              taskCard(currentTasks[index]),
+              Row(children: [
+                const Spacer(),
+                ElevatedButton(
+                  child: const Text('Delete'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                  ),
+                  onPressed: () {
+                    Task t = currentTasks[index];
+                    setState(() {
+                      currentTasks.removeAt(index);
+                    });
+                    currentUser.tasks.remove(t);
+                    DatabaseHelper.instance.removeTask(t.id);
+                  },
+                ),
+                const SizedBox(width: 2.0),
+                ElevatedButton(
+                  child: const Text('Complete'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                  ),
+                  onPressed: () {
+                    Task t = currentTasks[index];
+                    setState(() {
+                      currentTasks.removeAt(index);
+                    });
+                    DatabaseHelper.instance.completeTask(t.id);
+                    currentUser.points += 5;
+                    currentUser.completedTasks.add(currentUser.tasks[index]);
+                  },
+                ),
+                const SizedBox(width: 16.0),
+              ]),
+            ]);
+          },
+        ),
+      ),
+      pressed ? const CreateTasksForm() : const SizedBox(),
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            primary: const Color(0xFFF29765),
+            textStyle: const TextStyle(
+                color: Color(0xFFFFFFFF),
+                fontSize: 15,
+                fontWeight: FontWeight.bold)),
+        child: pressed ? const Text("Go Back") : const Text("Add Task"),
+        onPressed: () {
+          setState(() {
+            pressed = !pressed;
+          });
+        },
       ),
     ]);
   }
-
-  //       return const Center(child: CircularProgressIndicator());
-  //     },
-  //   );
-  // }
 }
 
 class CreateTasksForm extends StatefulWidget {
@@ -153,79 +157,51 @@ class CreateTasksFormState extends State<CreateTasksForm> {
         children: [
           const SizedBox(height: 20),
           Container(
-            padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+            padding: const EdgeInsets.fromLTRB(10, 0, 50, 0),
             child: TextFormField(
               controller: nameController,
               validator: nameValidator.validate,
               decoration: const InputDecoration(
                 labelText: 'Task Name',
+                icon: Icon(Icons.create_outlined),
               ),
             ),
           ),
           const SizedBox(
             height: 20,
           ),
-          // Container(
-          //   padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-          //   child: TextFormField(
-          //     controller: datetimeController,
-          //     validator: datetimeValidator.validate,
-          //     decoration: const InputDecoration(
-          //       labelText: 'Date and Time',
-          //     ),
-          //   ),
-          // ),
-          const SizedBox(
-            height: 20,
-          ),
           Container(
-            padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+            padding: const EdgeInsets.fromLTRB(10, 0, 50, 0),
             child: TextFormField(
               controller: descriptionController,
               validator: descriptionValidator.validate,
               decoration: const InputDecoration(
                 labelText: 'Description',
+                icon: Icon(Icons.notes),
               ),
             ),
           ),
           const SizedBox(
             height: 20,
           ),
-          // DateTimePicker(
-          //   type: DateTimePickerType.date,
-          //   //dateMask: 'yyyy/MM/dd',
-          //   // controller: _controller3,
-          //   //initialValue: _initialValue,
-          //   firstDate: year,
-          //   lastDate: DateTime(2100),
-          //   icon: Icon(Icons.event),
-          //   dateLabelText: 'Date',
-          //   locale: Locale('en', 'US'),
-          //   //onChanged: (val) => setState(() => _valueChanged3 = val),
-          //   // validator: (val) {
-          //   //  setState(() => _valueToValidate3 = val ?? '');
-          //   //  return null;
-          //   //  },
-          //   //  onSaved: (val) => setState(() => _valueSaved3 = val ?? ''),
-          // ),
-          DateTimePicker(
-            type: DateTimePickerType.dateTime,
-            dateMask: 'MMMM d, yyyy - h:mm a',
-            controller: datetimeController,
-            //timePickerEntryModeInput: true,
-            //controller: _controller4,
-            firstDate: DateTime(2021),
-            lastDate: DateTime(2100),
-            icon: Icon(Icons.event),
-            dateLabelText: 'Date Time',
-            use24HourFormat: false,
-            locale: Locale('en', 'US'),
-            validator: datetimeValidator.validate,
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 0, 50, 0),
+            child: DateTimePicker(
+              type: DateTimePickerType.dateTime,
+              dateMask: 'MMMM d, yyyy - h:mm a',
+              controller: datetimeController,
+              //timePickerEntryModeInput: true,
+              //controller: _controller4,
+              firstDate: DateTime(2021),
+              lastDate: DateTime(2100),
+              icon: const Icon(Icons.event),
+              dateLabelText: 'Date and Time',
+              use24HourFormat: false,
+              locale: const Locale('en', 'US'),
+              validator: datetimeValidator.validate,
+            ),
           ),
-
-
-
-          SizedBox(height: 30),
+          const SizedBox(height: 30),
           ElevatedButton(
               style: ElevatedButton.styleFrom(
                 primary: const Color(0xFFF29765),
@@ -233,36 +209,27 @@ class CreateTasksFormState extends State<CreateTasksForm> {
               child: const Text(
                 'Create task',
                 style: TextStyle(
-                  color: const Color(0xFFFFFFFF),
+                  color: Color(0xFFFFFFFF),
                   fontSize: 15,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               onPressed: () async {
-
-
                 if (_formKey.currentState!.validate()) {
-                  Task task = Task(
-                    currentUser.taskCount,
-                    nameController.text,
-                    datetimeController.text,
-                    descriptionController.text,
-                    currentUser.id,
-                  );
-
-                  addTask(currentUser, task);
 
                   await DatabaseHelper.instance.addTask(
                     //I HAVE NO IDEA HOW TO CALL THE FUNCTION FROM THE DATABASE HELPER LOL PLS HELP
                     nameController.text, datetimeController.text,
                     descriptionController.text, currentUser.id,
                   );
+
+                  currentUser.tasks = await DatabaseHelper.instance
+                      .getTasksByUser(currentUser.id);
                   showDialog(
                     context: context,
                     builder: (BuildContext context) {
                       return AlertDialog(
-                        title: const Text(
-                            "Successfully added task."),
+                        title: const Text("Successfully added task."),
                         actions: <Widget>[
                           TextButton(
                             child: const Text('OK'),
